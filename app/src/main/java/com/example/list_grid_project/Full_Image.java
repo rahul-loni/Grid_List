a0d5c57491fc35a59db1cb8fd2413c2ec8fb41b7
Rahul Kumar <rahul@gmail.com>
2024-08-12 08:36:17 +0545

    Grid , List and Menu

--- app/src/main/java/com/example/list_grid_project/Full_Image.java
+++ app/src/main/java/com/example/list_grid_project/Full_Image.java
@@ -1,6 +1,8 @@
 package com.example.list_grid_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,19 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class Full_Image extends AppCompatActivity {
-
+     ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_full_image);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        imageView=findViewById(R.id.fullImage);
+
+        Intent i=getIntent();
+
+        int position=i.getExtras().getInt("id");
+        ImageAdapter imageAdapter=new ImageAdapter(this);
+        imageView.setImageResource(imageAdapter.images[position]);
     }
 }
--- app/src/main/java/com/example/list_grid_project/GridPage.java
+++ app/src/main/java/com/example/list_grid_project/GridPage.java
@@ -1,6 +1,11 @@
 package com.example.list_grid_project;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +14,28 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class GridPage extends AppCompatActivity {
-
+    GridView gridView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_grid_page);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+
+        gridView=findViewById(R.id.grid);
+
+        //Get Data and Set Data To GridView
+        gridView.setAdapter(new ImageAdapter(this));
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(GridPage.this, "Click Image ", Toast.LENGTH_SHORT).show();
+                Intent intent=new Intent(GridPage.this,Full_Image.class);
+                intent.putExtra("id",i);
+                startActivity(intent);
+            }
         });
+
     }
 }
